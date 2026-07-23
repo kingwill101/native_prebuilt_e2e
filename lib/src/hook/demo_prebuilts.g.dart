f59e59befd7eff3508c971b4be918562ecd08727
kingwill101 <hey@glenfordwilliams.com>
2026-07-22 21:44:10 -0500

    Publish e2e release assets and update manifest

--- lib/src/hook/demo_prebuilts.g.dart
+++ lib/src/hook/demo_prebuilts.g.dart
@@ -5,12 +5,24 @@ import 'package:native_prebuilt/native_prebuilt.dart';
 
 const native_prebuilt_e2ePrebuilts = PrebuiltManifest(
   schemaVersion: 1,
-  release: GitLabReleaseSource(projectPath: 'kingwill101/native_prebuilt_e2e', tag: 'native_prebuilt_e2e-v0.0.6'),
+  release: GitHubReleaseSource(owner: 'kingwill101', repository: 'native_prebuilt_e2e', tag: 'native_prebuilt_e2e-v0.0.10'),
   artifacts: {
     'linux-x64': PrebuiltArtifact(
       archiveName: 'native_prebuilt_e2e-linux-x64.tar.gz',
-      archiveSha256: '4eaa8ab50762443af01bd65e03556758dc8d21c1e8f37dcdeb53a83f3de31d08',
-      payloadSha256: 'f90fc7acfb4bedf66d99f6306e51a8eeac5cf34013d4e5f1c20db4ade754a659',
+      archiveSha256: 'b42f43ad81d43c44f1056be3e9775cd89cff451477e414357e217325a5dce288',
+      payloadSha256: 'cd7819b8576818e06fa5be1bebe612e792469be842162771075553e30822c24a',
+      payload: DynamicLibraryPayload(libraryStem: 'native_prebuilt_e2e', acceptVersionedNames: true),
+    ),
+    'windows-x64': PrebuiltArtifact(
+      archiveName: 'native_prebuilt_e2e-windows-x64.tar.gz',
+      archiveSha256: '88d4d9a2cf906e27904918b2a3882cb691598e7f4cc37ab0818284e3db6b4235',
+      payloadSha256: '0469fbaea9d26d540bea5436f23cdb9b3bbb187ce7bcbd65b460736738e8ceea',
+      payload: DynamicLibraryPayload(libraryStem: 'native_prebuilt_e2e', acceptVersionedNames: true),
+    ),
+    'macos-arm64': PrebuiltArtifact(
+      archiveName: 'native_prebuilt_e2e-macos-arm64.tar.gz',
+      archiveSha256: 'd68f805a90fcfedb74523717e8c6bb96a5fb009c8d6e6fbe1cac26ad9c5daae1',
+      payloadSha256: 'd135240cb4573f4fdfef50e0e8f5af261f1255004345cc3cc6c434111c157b05',
       payload: DynamicLibraryPayload(libraryStem: 'native_prebuilt_e2e', acceptVersionedNames: true),
     ),
   },
